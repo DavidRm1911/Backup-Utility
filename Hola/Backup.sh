74c19f067fc433ca1f6a6e32ecd0146746801872
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-01-18 19:58:45 -0500

    Terminadoooo azure

--- Hola/Backup.sh
+++ Hola/Backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Directorio de origen que deseas respaldar
-origen="/ruta/al/directorio/a/respaldar"
+origen="/home/rumizaske/Desktop/Adios/"
 
 # Directorio de destino donde se almacenarán las copias de seguridad
-destino="/ruta/al/directorio/de/respaldo"
+destino="/home/rumizaske/Desktop/LugarSeguro/"
 
 # Nombre del archivo de copia de seguridad con marca de tiempo
 archivo_destino="$destino/respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
@@ -14,7 +14,10 @@ tar -czvf "$archivo_destino" "$origen"
 
 # Comprobar si la copia de seguridad se realizó con éxito
 if [ $? -eq 0 ]; then
-  echo "Copia de seguridad exitosa en $archivo_destino"
+ 
+ echo "Copia de seguridad exitosa en $archivo_destino"
+	sshpass -p 'Rumizaske123' rsync -arv "$archivo_destino" rumi@20.14.80.158:~/Desktop --log-file=rsync_log.txt
+
 else
   echo "Error al realizar la copia de seguridad."
 fi
